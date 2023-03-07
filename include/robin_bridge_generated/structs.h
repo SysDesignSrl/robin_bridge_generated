436d5543c0b6cb02e9f7a2147910c744dc0ec451
Nicola Conti <nicola@sysdesign.it>
2023-03-07 14:09:56 +0100

    Add CommonCommand CommonFeedback messages

--- include/robin_bridge_generated/structs.h
+++ include/robin_bridge_generated/structs.h
@@ -1,4 +1,18 @@
 #ifndef ROBIN_STRUCTS_H
 #define ROBIN_STRUCTS_H
 #include <cstdint>
+struct CommonCommand
+{
+  uint8_t execute;
+  uint8_t abort;
+  int32_t id;
+};
+struct CommonFeedback
+{
+  uint8_t busy;
+  uint8_t done;
+  uint8_t aborted;
+  uint8_t error;
+  int32_t error_id;
+};
 #endif
